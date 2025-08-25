80a3c8335b7a6a57c3ef23b469d3a7f715e5e705
Vinicius <vinicius.dantas.336@gmail.com>
2025-08-25 20:07:07 -0300

    Sistema para bater ponto V1.0

--- Sistema.java
+++ Sistema.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class teste {
+public class Sistema {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
